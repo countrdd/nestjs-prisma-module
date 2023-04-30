3b72e911ee813b51d736fda8423c6787fc3930c5
dustiancountryman <countrdd@leillc.net>
2023-04-30 07:01:42 -0400

    ddfff

---
+++ dist/connection.factory.d.ts
@@ -0,0 +1,9 @@
+import type { FactoryProvider } from '@nestjs/common';
+import type PrismaService from './prisma.service';
+import type { ClassLike } from './types';
+declare const _default: (
+  name: string,
+  multitenancy: boolean,
+  _service: PrismaService<ClassLike>,
+) => FactoryProvider;
+export default _default;
